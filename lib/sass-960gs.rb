4021fb309beb26768d6aa2530fc731d6cacee649
Łukasz Niemier <lukasz@niemier.pl>
2012-07-06 13:52:43 +0200

    Update to version 0.1.0

--- lib/sass-960gs.rb
+++ lib/sass-960gs.rb
@@ -1,6 +1,8 @@
-module Sass960gs
-  module Rails
-    class Engine < ::Rails::Engine
+if defined? Rails
+  module Sass960gs
+    module Rails
+      class Engine < ::Rails::Engine
+      end
     end
   end
 end
